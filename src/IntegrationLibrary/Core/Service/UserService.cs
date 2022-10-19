dd90c82224b9e6459cfd307661e7b97b703eb07f
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-10-19 21:18:03 +0200

    integration projects init

---
+++ src/IntegrationLibrary/Core/Service/UserService.cs
@@ -0,0 +1,27 @@
+﻿using IntegrationLibrary.Core.Model;
+using IntegrationLibrary.Core.Repository;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntegrationLibrary.Core.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Register(User user)
+        {
+            _userRepository.Register(user);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _userRepository.GetAll();
+        }
+    }
+}
---
+++ src/IntegrationLibrary/Settings/IntegrationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IntegrationLibrary.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationLibrary.Settings
+{
+    public class IntegrationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public IntegrationDbContext(DbContextOptions<IntegrationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User() { Id = 1, Email = "email1@gmail.com", AppName = "app1", Password = "pw1", Server = "localhost:5555" },
+                new User() { Id = 2, Email = "email2@gmail.com", AppName = "app2", Password = "pw2", Server = "localhost:6555" },
+                new User() { Id = 3, Email = "email3@gmail.com", AppName = "app3", Password = "pw3", Server = "localhost:7555" }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
