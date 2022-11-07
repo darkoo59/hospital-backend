cb9b2a74c0463293d71975b09d5181c0b1640672
Bosko Ristovic <malibolence@gmail.com>
2022-11-07 14:26:31 +0100

    Separate RoomMap from Room logic

---
+++ src/HospitalLibrary/Core/Service/RoomMapService.cs
@@ -0,0 +1,39 @@
+﻿using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Service
+{
+    public class RoomMapService : IRoomMapService
+    {
+        private readonly IRoomMapRepository _roomMapRepository;
+
+        public RoomMapService(IRoomMapRepository roomMapRepository)
+        {
+            _roomMapRepository = roomMapRepository;
+        }
+        public IEnumerable<RoomMap> GetAll()
+        {
+            return _roomMapRepository.GetAll();
+        }
+
+        public RoomMap GetById(int id)
+        {
+            return _roomMapRepository.GetById(id);
+        }
+
+        public RoomMap GetByNumber(string number)
+        {
+            return _roomMapRepository.GetByNumber(number);
+        }
+
+        public IEnumerable<RoomMap> GetRooms(string buildingId, int floorId)
+        {
+            return _roomMapRepository.GetRooms(buildingId, floorId);
+        }
+    }
+}
