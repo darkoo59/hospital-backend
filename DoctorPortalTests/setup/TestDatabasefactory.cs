a099a0905d49613348bd0413766c183faf32c64b
Bosko Ristovic <malibolence@gmail.com>
2022-11-21 12:42:16 +0100

    add tests for equipment getting

--- DoctorPortalTests/setup/TestDatabasefactory.cs
+++ DoctorPortalTests/setup/TestDatabasefactory.cs
@@ -1,5 +1,7 @@
 ﻿using HospitalAPI;
 using HospitalLibrary.Core.Model;
+using HospitalLibrary.HospitalMap.Enums;
+using HospitalLibrary.HospitalMap.Model;
 using HospitalLibrary.Settings;
 using HospitalLibrary.SharedModel;
 using Microsoft.AspNetCore.Hosting;
@@ -36,7 +38,7 @@ namespace HospitalTests.setup
 
         private static string CreateConnectionStringForTest()
         {
-            return "Host=localhost;Database=HospitalTestDb;Username=postgres;Password=marko;";
+            return "Host=localhost;Database=HospitalTestDb;Username=postgres;Password=password;";
         }
 
         private static void InitializeDatabase(HospitalDbContext context)
@@ -70,6 +72,17 @@ namespace HospitalTests.setup
 
             context.BloodUsageEvidencies.Add(new BloodUsageEvidency() { BloodUsageEvidencyId = 1, BloodType = BloodType.A_PLUS, QuantityUsedInMililiters = 200, DateOfUsage = new System.DateTime(2022, 12, 13), ReasonForUsage = "Hearth surgery", DoctorId = 1 });
 
+            context.Database.ExecuteSqlRaw("TRUNCATE TABLE  \"Equipment\";");
+
+            context.Equipment.Add(new Equipment() { EquipmentType = EquipmentType.Dynamic, Id = 1, RoomId = 1, Name = "Syringe", Quantity = 50 });
+            context.Equipment.Add(new Equipment() { EquipmentType = EquipmentType.Dynamic, Id = 2, RoomId = 1, Name = "Tounge depressor", Quantity = 32 });
+            context.Equipment.Add(new Equipment() { EquipmentType = EquipmentType.Dynamic, Id = 3, RoomId = 2, Name = "Gloves", Quantity = 50 });
+
+/*            context.Database.ExecuteSqlRaw("DROP TABLE  \"Beds\";");
+            context.Database.ExecuteSqlRaw("TRUNCATE TABLE  \"Rooms\";");
+
+            context.Rooms.Add(new Room() { Id = 1, Number = "101A", FloorId = 0, BuildingId = "A", Type = RoomType.AppointmentRoom, Description = "neki opis", X = 10, Y = 10, Width = 380, Height = 250 });
+            context.Rooms.Add(new Room() { Id = 2, Number = "102A", FloorId = 0, BuildingId = "A", Type = RoomType.AppointmentRoom, Description = "neki opis1", X = 10, Y = 270, Width = 170, Height = 250 });*/
             context.SaveChanges();
          }
     }
