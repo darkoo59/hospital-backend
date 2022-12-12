d405d28409ab7da021e632ef89eb25af2b60f879
VojinBjelica <vojinb111@gmail.com>
2022-12-12 13:08:12 +0100

    Add e2e test for blood requests

---
+++ IntegrationTests/E2E/BloodRequest/BloodRequestTests.cs
@@ -0,0 +1,70 @@
+﻿using IntegrationTests.E2E.BloodRequest.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Priority;
+
+namespace IntegrationTests.E2E.BloodRequest
+{
+    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
+    [Collection("collection")]
+    public class BloodRequestTests
+    {
+        private readonly IWebDriver Driver;
+        private readonly Pages.BloodRequestPage BloodRequestPage;
+        private readonly Pages.BloodRequestAcceptedPage BloodRequestAcceptedPage;
+        private readonly bool TEST_DISABLED = false;
+
+        public BloodRequestTests()
+        {
+            if (TEST_DISABLED) return;
+
+            ChromeOptions options = new();
+            options.AddArguments("start-maximized");            // open Browser in maximized mode
+            options.AddArguments("disable-infobars");           // disabling infobars
+            options.AddArguments("--disable-extensions");       // disabling extensions
+            options.AddArguments("--disable-gpu");              // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-notifications");    // disable notifications
+
+            Driver = new ChromeDriver(options);
+
+            BloodRequestPage = new Pages.BloodRequestPage(Driver);
+            BloodRequestAcceptedPage = new BloodRequestAcceptedPage(Driver);
+            BloodRequestPage.Navigate();
+            BloodRequestPage.EnsurePageIsDisplayed();
+
+            Assert.True(BloodRequestPage.GridDisplayed());
+        }
+
+        private void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+
+        [Fact, Priority(1)]
+        public void TestApproveRequest()
+        {
+            if (TEST_DISABLED) return;
+
+            int newRequests = BloodRequestPage.CountGridElements();
+
+            BloodRequestPage.Accept();
+
+            var el = new WebDriverWait(Driver, TimeSpan.FromSeconds(3));
+
+            int requestsAfterApprove = BloodRequestPage.CountGridElements();
+
+            Assert.Equal(newRequests - 1, requestsAfterApprove);
+
+        }
+    }
+}
---
+++ IntegrationTests/E2E/BloodRequest/Pages/BloodRequestPage.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.E2E.BloodRequest.Pages
+{
+    public class BloodRequestPage
+    {
+        private readonly IWebDriver Driver;
+        public const string URI = "http://localhost:4200/manager/blood-req/new";
+
+        public string Title => Driver.Title;
+
+        private IWebElement Grid => Driver.FindElement(By.ClassName("grid"));
+        private ICollection<IWebElement> ApproveBtn => Driver.FindElements(By.ClassName("approveBtn"));
+
+        public BloodRequestPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(Driver, new TimeSpan(0, 0, 20));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return GridDisplayed();
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public bool GridDisplayed()
+        {
+            return Grid.Displayed;
+        }
+
+        public int CountGridElements()
+        {
+            return Driver.FindElements(By.ClassName("approveBtn")).Count;
+        }
+
+        public void Accept()
+        {
+            ApproveBtn.FirstOrDefault().Click(); 
+        }
+
+        public void Navigate() => Driver.Navigate().GoToUrl(URI);
+    }
+}
