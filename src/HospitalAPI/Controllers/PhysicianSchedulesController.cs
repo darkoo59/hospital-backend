ce743b9eb8d9d59e082de4a8a542337c9ad690d5
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2023-01-03 16:30:28 +0100

    Fix problems

---
+++ src/HospitalAPI/Controllers/PhysicianSchedulesController.cs
@@ -0,0 +1,116 @@
+﻿using HospitalAPI.Dtos;
+using HospitalAPI.Mappers;
+using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhysicianSchedulesController : ControllerBase
+    {
+        private readonly IPhysicianScheduleService _service;
+        private readonly IGenericMapper<PhysicianSchedule, PhysicianScheduleDTO> _mapper;
+        private readonly IGenericMapper<Appointment, AppointmentDTO> _appointmentMapper;
+
+        public PhysicianSchedulesController(IPhysicianScheduleService service, IGenericMapper<PhysicianSchedule, PhysicianScheduleDTO> mapper, IGenericMapper<Appointment, AppointmentDTO> appointmentMapper)
+        {
+            _service = service;
+            _mapper = mapper;
+            _appointmentMapper = appointmentMapper;
+        }
+
+        [HttpPost]
+        public ActionResult Create(PhysicianScheduleDTO physicianScheduleDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            PhysicianSchedule physicianSchedule = _mapper.ToModel(physicianScheduleDTO);
+            _service.Create(physicianSchedule);
+            return CreatedAtAction("GetById", new { id = physicianSchedule.PhysicianScheduleId }, physicianSchedule);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            var physicianSchedyle = _service.GetById(id);
+            if (physicianSchedyle == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.ToDTO(physicianSchedyle));
+        }
+
+        [HttpGet("doctor/{doctorId}")]
+        public ActionResult GetPhysicianSchedule(int doctorId)
+        {
+            return Ok(_mapper.ToDTO(_service.Get(doctorId)));
+        }
+
+        [HttpGet("appointments/{doctorId}")]
+        public ActionResult GetAppointments(int doctorId)
+        {
+            return Ok(_appointmentMapper.ToDTO(_service.GetAppointments(doctorId)));
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok(_mapper.ToDTO(_service.GetAll().ToList()));
+        }
+
+        [HttpPost("create")]
+        public ActionResult Create()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            List<WorkTime> workTimes = new List<WorkTime>();
+            workTimes.Add(new WorkTime(new DateRange(new DateTime(2022, 12, 8), new DateTime(2022, 12, 25)), new DateTime(2022, 11, 11, 7, 0, 0), new DateTime(2022, 11, 11, 15, 0, 0)));
+            List<Appointment> appointments = new List<Appointment>();
+            appointments.Add(new Appointment(new DateRange(new DateTime(2022, 12, 8, 7, 0, 0), new DateTime(2022, 12, 8, 7, 30, 0)), 1, null, 1, null, false));
+            PhysicianSchedule physicianSchedule = new PhysicianSchedule(1, 1, null, workTimes, appointments, new List<Vacation>());
+            _service.Create(physicianSchedule);
+            return CreatedAtAction("GetById", new { id = physicianSchedule.PhysicianScheduleId }, physicianSchedule);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, PhysicianSchedule physicianSchedule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != physicianSchedule.PhysicianScheduleId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _service.Update(physicianSchedule);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return Ok(physicianSchedule);
+        }
+
+
+    }
+}
