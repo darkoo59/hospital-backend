04f7deb3fa15bbd2f8ded11da36c19557e4c067e
Rade Stojanovic <stojanovicrade614@gmail.com>
2023-01-09 11:25:34 +0100

    sftp setup

--- src/IntegrationLibrary/Migrations/IntegrationDbContextModelSnapshot.cs
+++ src/IntegrationLibrary/Migrations/IntegrationDbContextModelSnapshot.cs
@@ -318,7 +318,7 @@ namespace IntegrationLibrary.Migrations
                         {
                             Id = 1,
                             Description = "Congue nisi vitae suscipit tellus mauris. Et leo duis ut diam quam nulla. Porttitor eget dolor morbi non arcu risus quis. Tempor nec feugiat nisl pretium. Pharetra et ultrices neque ornare aenean euismod elementum nisi. Dui sapien eget mi proin sed libero enim sed faucibus. Vitae turpis massa sed elementum tempus. Urna molestie at elementum eu facilisis sed. Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Facilisi cras fermentum odio eu feugiat. Rhoncus aenean vel elit scelerisque. Eget nunc scelerisque viverra mauris in aliquam. Blandit libero volutpat sed cras ornare. Tellus elementum sagittis vitae et leo duis. Est lorem ipsum dolor sit amet consectetur. Ullamcorper malesuada proin libero nunc consequat interdum varius.",
-                            ExpiresOn = new DateTime(2022, 12, 27, 8, 59, 56, 137, DateTimeKind.Local).AddTicks(5481),
+                            ExpiresOn = new DateTime(2023, 2, 8, 11, 19, 19, 512, DateTimeKind.Local).AddTicks(3151),
                             State = 0,
                             Title = "Tender 1"
                         },
@@ -326,7 +326,7 @@ namespace IntegrationLibrary.Migrations
                         {
                             Id = 2,
                             Description = "Egestas congue quisque egestas diam in. Pretium aenean pharetra magna ac placerat. Ultrices neque ornare aenean euismod. Eget felis eget nunc lobortis mattis aliquam faucibus purus. Ac feugiat sed lectus vestibulum. Mi proin sed libero enim sed faucibus turpis in eu. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Enim ut tellus elementum sagittis vitae et.",
-                            ExpiresOn = new DateTime(2022, 12, 27, 8, 59, 56, 150, DateTimeKind.Local).AddTicks(4343),
+                            ExpiresOn = new DateTime(2023, 2, 8, 11, 19, 19, 515, DateTimeKind.Local).AddTicks(7492),
                             State = 0,
                             Title = "Tender 2"
                         },
@@ -334,7 +334,7 @@ namespace IntegrationLibrary.Migrations
                         {
                             Id = 3,
                             Description = "Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Facilisi cras fermentum odio eu feugiat. Rhoncus aenean vel elit scelerisque. Eget nunc scelerisque viverra mauris in aliquam. Blandit libero volutpat sed cras ornare. Tellus elementum sagittis vitae et leo duis. Est lorem ipsum dolor sit amet consectetur. Ullamcorper malesuada proin libero nunc consequat interdum varius.",
-                            ExpiresOn = new DateTime(2022, 12, 27, 8, 59, 56, 150, DateTimeKind.Local).AddTicks(4832),
+                            ExpiresOn = new DateTime(2023, 2, 8, 11, 19, 19, 515, DateTimeKind.Local).AddTicks(7717),
                             State = 0,
                             Title = "Tender 3"
                         });
@@ -349,6 +349,9 @@ namespace IntegrationLibrary.Migrations
 
                     b.Property<int>("EquipmentTenderId")
                         .HasColumnType("integer");
+
+                    b.Property<DateTime>("Finished")
+                        .HasColumnType("timestamp without time zone");
 
                     b.Property<bool>("HasWon")
                         .HasColumnType("boolean");
